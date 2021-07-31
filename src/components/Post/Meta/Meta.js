ffc7ba703e6f0b805682c5e05b849b84d70a07ba
m7mdra <xm7mdrax@gmail.com>
2021-07-31 23:40:55 +0200

    fixes

--- src/components/Post/Meta/Meta.js
+++ src/components/Post/Meta/Meta.js
@@ -8,8 +8,12 @@ type Props = {
 
 const Meta = ({ date }: Props) => (
   <div className={styles['meta']}>
-    <p className={styles['meta__date']}>Published {new Date(date).toLocaleDateString('en-US', { year: 'numeric', month: 'short', day: 'numeric' })}</p>
+    <p className={styles['meta__date']}>تم النشر في  {new Date(date).toLocaleDateString('AR', { year: 'numeric', month: 'short', day: 'numeric' })}</p>
+    <div>
+      اذا اعجبك هذا المقال فلربما يستفيد من شخص اخر اذا قمت بمشاركته
   </div>
+  </div>
+  
 );
 
 export default Meta;
